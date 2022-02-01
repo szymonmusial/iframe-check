6b89f85e29ad74e95c08748fbf5d33f394a71a96
szymonmusial <szymonmusial.sm@gmail.com>
2022-02-01 10:09:40 +0100

    done basic iFrame

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <show-iframe src="https://www.musialdev.pl/" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShowIframe from "./components/atoms/ShowIframe.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { ShowIframe },
+  name: "App",
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.app {
+  width: 100%;
+}
+
+body {
+  margin: 0px;
+  padding: 0px;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/atoms/ShowIframe.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="iframe">
+    <iframe :src="src" class="intranet" frameborder="0" width="100%"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: String,
+  },
+};
+</script>
+
+<style scoped>
+iframe {
+  width: 100%;
+  height: 100vh;
+}
+</style>
